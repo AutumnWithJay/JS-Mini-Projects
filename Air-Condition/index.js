1264e94b4f089149dcd82d520f8de77632831581
Jay Park <devpark.0x1c@gmail.com>
2021-05-13 19:41:56 +0900

    Air Condition: Change Icon by weather Condition and change HTML Structure

--- Air-Condition/index.js
+++ Air-Condition/index.js
@@ -1,29 +1,107 @@
-const locationName = document.querySelector('.location');
-const weather = document.querySelector('.weather');
-const pollution = document.querySelector('.pollution');
-
+// Current Container Variable
+const currentTime = document.querySelector('.current-time');
+const locationName = document.querySelector('.current-location');
+// Weather Variable
+const weatherIcon = document.querySelector('.weather-icon');
+const temperture = document.querySelector('.weather-temperture');
+// Air Pollution Variable
+const fineDustIcon = document.querySelector('.fine-dust__icon');
+const fineDustTitle = document.querySelector('.fine-dust__title');
+const fineDustValue = document.querySelector('.fine-dust__value');
+const ultraFineDustIcon = document.querySelector('.ultra-fine-dust__icon');
+const ultraFineDustTitle = document.querySelector('.ultra-fine-dust__title');
+const ultraFineDustValue = document.querySelector('.ultra-fine-dust__value');
+// API KEY
 const API_KEY = '039716d966943254bee4c969a2930cc0';
 
+let isAirLoading = true;
+let isWeatherLoading = true;
+
+const iconObj = {
+  high: '<i class="far fa-smile"></i>',
+  low: '<i class="fas fa-temperature-low"></i>',
+  half: '<i class="fas fa-thermometer-half"></i>',
+  great: '<i class="far fa-grin-hearts"></i>',
+  soso: '<i class="far fa-grin"></i>',
+  bad: '<i class="fas fa-tired"></i>',
+};
+
+const validateIcon = (kind, info1, info2) => {
+  const { high, low, half, great, soso, bad } = iconObj;
+
+  if (kind === 'weather') {
+    if (info1 > 26) {
+      weatherIcon.innerHTML = high;
+    } else if (info1 > 13) {
+      weatherIcon.innerHTML = half;
+    } else {
+      weatherIcon.innerHTML = low;
+    }
+    weatherIcon.style.color = 'red';
+  } else {
+    if (info1 > 100) {
+      fineDustIcon.innerHTML = bad;
+      fineDustIcon.style.color = 'black';
+    } else if (info1 > 50) {
+      fineDustIcon.innerHTML = soso;
+      fineDustIcon.style.color = 'green';
+    } else {
+      fineDustIcon.innerHTML = great;
+      fineDustIcon.style.color = 'blue';
+    }
+    if (info2 > 100) {
+      ultraFineDustIcon.innerHTML = bad;
+      ultraFineDustIcon.style.color = 'black';
+    } else if (info2 > 50) {
+      ultraFineDustIcon.innerHTML = soso;
+      ultraFineDustIcon.style.color = 'green';
+    } else {
+      ultraFineDustIcon.innerHTML = great;
+      ultraFineDustIcon.style.color = 'blue';
+    }
+  }
+};
+
 const getAirpollution = (lat, lon) => {
-  const URL = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&lang=kr&appid=${API_KEY}`;
+  const URL = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
   fetch(URL)
     .then((res) => res.json())
     .then((res) => {
       const { pm2_5, pm10 } = res.list[0].components;
+
+      fineDustValue.textContent = `${pm2_5}`;
+      ultraFineDustValue.textContent = `${pm10}`;
+      validateIcon('pollution', pm2_5, pm10);
+      isAirLoading = false;
     });
 };
 
 const getWeatherCondition = (lat, lon) => {
-  const URL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=kr&appid=${API_KEY}`;
+  const URL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
   fetch(URL)
     .then((res) => res.json())
     .then((res) => {
       const {
-        main: { temp, feels_like, humidity },
-        wind: { speed },
-        weather: { description },
+        name,
+        main: { temp },
       } = res;
+
+      locationName.textContent = `${name}`;
+      temperture.textContent = `${Math.ceil(temp - 273.99)}℃`;
+      validateIcon('weather', Math.ceil(temp - 273.99));
+      isWeatherLoading = false;
     });
+};
+
+const displayTime = () => {
+  const today = new Date();
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds();
+
+  currentTime.textContent = `
+  ${hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}
+  `;
 };
 
 const getLocation = (pos) => {
@@ -40,6 +118,11 @@ const responseError = () => {
 
 const init = () => {
   navigator.geolocation.getCurrentPosition(getLocation, responseError);
+  setInterval(() => {
+    if (!isAirLoading && !isWeatherLoading) {
+      displayTime();
+    }
+  }, 1000);
 };
 
 init();
